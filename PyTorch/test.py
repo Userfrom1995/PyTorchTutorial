47a8633a9b8ad75600921c3af6a100982c77e249
USER1995 <userfrom1995@gmail.com>
2024-06-05 17:46:18 +0530

    Synced Everything Again

--- PyTorch/test.py
+++ PyTorch/test.py
@@ -1,1 +1,14 @@
-TEST
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Generate data
+x = np.linspace(0, 2*np.pi, 100)
+y = np.sin(x)
+
+# Plot the data
+plt.plot(x, y)
+plt.title('Sine Wave')
+plt.xlabel('x')
+plt.ylabel('sin(x)')
+plt.grid(True)
+plt.show()
